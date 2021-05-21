02b3cb3728d7f65f439febcf5ed420828bf678ed
dipanshu72 <dipanshudiwakar@gmail.com>
2021-05-21 15:50:23 +0530

    commit 3
    completion done

--- Calculator/src/dipanshu/StringCalculator.java
+++ Calculator/src/dipanshu/StringCalculator.java
@@ -1,6 +1,9 @@
 package dipanshu;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StringCalculator {
@@ -13,21 +16,35 @@ public class StringCalculator {
 		this.numbers = numbers;
 		
 	}
-       private int sum() {
+       private int sum() {	
+    	   ensureNoNegativeNumbers();
+    	   
+    	   return getNumbers().sum();
 		
-		return Arrays.stream(numbers.split(delimiter))
-				.mapToInt(Integer::parseInt)
-				.sum();
 		}
 	
 	
+	private void ensureNoNegativeNumbers() {
+		String negativenumberSequence = getNumbers().filter(n -> n<0)
+				.mapToObj(Integer::toString)
+				.collect(Collectors.joining(","));	
+		if(!negativenumberSequence.isEmpty()) {					
+ 		   throw new IllegalArgumentException("negative number: "+ negativenumberSequence);
+ 	   }		
+	}
+	private IntStream getNumbers() {
+		if(numbers.isEmpty()) {
+			return IntStream.empty();
+		}else {
+		
+		return Stream.of(numbers.split(delimiter))
+				.mapToInt(Integer::parseInt)
+				.map(n -> n%1000);
+	}
+	}
 	public static int sum(String input) {
-		if(input.isEmpty())
-		   return 0;
 		
-		return parseInput(input).sum();
-		
-		
+		return parseInput(input).sum();		
 		
 	}
 
@@ -35,12 +52,24 @@ public class StringCalculator {
 	private static StringCalculator parseInput(String input) {
 		if (input.startsWith("//")) {
 			String[] ports = input.split("\n",2);
-			return new StringCalculator(ports[0].substring(2), ports[1]);
+			return new StringCalculator(parseDelimiter(ports[0]), ports[1]);
 			
 		} else {
 			return new StringCalculator(",|\n",input);
 		}
 	}
+	private static String parseDelimiter(String header) {
+		String delimiter = header.substring(2);
+		if(delimiter.startsWith("[")) {
+			delimiter = delimiter.substring(1, delimiter.length()-1);
+		}
+		return Stream.of(delimiter.split("]\\["))
+				.map(Pattern::quote)
+				.collect(Collectors.joining("|"));
+
+		
+	}
+	
 	
 
 }
--- Calculator/src/dipanshu/StringClalulatorTest.java
+++ Calculator/src/dipanshu/StringClalulatorTest.java
@@ -1,9 +1,13 @@
 package dipanshu;
 
 import static org.junit.Assert.assertThat;
+
+import org.junit.Rule;
+
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 public class StringClalulatorTest {
 	
 	@Test
@@ -42,6 +46,55 @@ public class StringClalulatorTest {
 	@Test
 	public void usesDelimiterSepcified() {
 		assertThat(StringCalculator.sum("//;\n1;2"), is(3));
+		assertThat(StringCalculator.sum("//.\n2.3.1"), is(6));
+
+		
 	}
+	
+	
+	
+	
+	@Rule
+	public ExpectedException expectedExpection = ExpectedException.none();
+	
+	@Test
+	public void throwsIfNegativeNumber() {
+		expectedExpection.expect(IllegalArgumentException.class);
+		expectedExpection.expectMessage("negative number: -3");
+
+		StringCalculator.sum("-3");
+	}
+	
+	@Test
+	public void throwsonNegitiveNumbersWithAllNumbersInExceptionMessage() {
+		expectedExpection.expect(IllegalArgumentException.class);
+		expectedExpection.expectMessage("negative number: -3,-2,-15");
+		
+		StringCalculator.sum("-3,1,5,-2,-15");
+
+	}
+	@Test
+	public void mapsNumberAbove1000ToLastThreeDigit() {
+		assertThat(StringCalculator.sum("1002"), is(2));
+		assertThat(StringCalculator.sum("1002,1057"), is(59));
+
+	}
+	
+	@Test
+	public void acceptDelimiterofArbitroryLength() {
+		assertThat(StringCalculator.sum("//[***]\n1***2***3"), is(6));
+
+	
+	}
+	
+	@Test
+	public void acceptsmultipleDelimiter() {
+		assertThat(StringCalculator.sum("//[*][%]\n1*2%3"), is(6));
+		assertThat(StringCalculator.sum("//[**][%%]\n1**2%%3"), is(6));
+	}
+	
+	
+	
+	
 
 }
